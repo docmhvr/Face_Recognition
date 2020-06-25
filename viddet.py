5de6698d2e19e3848e9329b9a76daeec4f1b337f
doc_mhvr <mahavir37chandaliya@gmail.com>
2020-06-25 18:24:39 +0530

    Removed indentation bug

--- viddet.py
+++ viddet.py
@@ -13,7 +13,7 @@ while True:
 	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors=5)
 	for x,y,w,h in faces:
-    	frame = cv2.rectangle(frame, (x,y), (x+w,y+h),(0,0,255),20)
+		frame = cv2.rectangle(frame, (x,y), (x+w,y+h),(0,0,255),20)
 
 	cv2.imshow('Capturing',frame)
 	key = cv2.waitKey(1)
